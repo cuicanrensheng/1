3a2982bef8daf87238ee6ad2e60a439613657b57
cuicanrensheng <1693728915@qq.com>
2026-06-04 09:20:45 +0800

    Update App.java

--- app/src/main/java/com/iptvlive/App.java
+++ app/src/main/java/com/iptvlive/App.java
@@ -1,7 +1,9 @@
 package com.iptvlive;
 
-import androidx.multidex.MultiDexApplication;
+import android.content.Context;
 import android.util.Log;
+import androidx.multidex.MultiDex;
+import androidx.multidex.MultiDexApplication;
 import com.iptvlive.util.AppSpUtil;
 import com.iptvlive.util.HttpHeaderSpUtil;
 import com.iptvlive.util.LogSpUtil;
@@ -10,8 +12,15 @@ import com.iptvlive.util.LogSpUtil;
  * APP全局Application，程序启动最先初始化
  * 初始化全部SP存储工具、全局崩溃捕获
  */
-// 父类从Application改为MultiDexApplication
 public class App extends MultiDexApplication {
+
+    //【必须新增】低版本安卓手动加载MultiDex，解决ClassNotFound
+    @Override
+    protected void attachBaseContext(Context base) {
+        super.attachBaseContext(base);
+        MultiDex.install(base);
+    }
+
     @Override
     public void onCreate() {
         super.onCreate();
