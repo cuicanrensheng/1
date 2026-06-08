51edfc514fd5d958cb27c10f2de40927bea6a5b4
cuicanrensheng <1693728915@qq.com>
2026-06-08 21:20:51 +0800

    Update EpgHelper.kt

--- app/src/main/java/com/tvlive/EpgHelper.kt
+++ app/src/main/java/com/tvlive/EpgHelper.kt
@@ -1,42 +1,50 @@
 package com.tvlive
 
-import okhttp3.OkHttpClient
-import okhttp3.Request
+import org.xmlpull.v1.XmlPullParser
 import org.xmlpull.v1.XmlPullParserFactory
+import java.io.StringReader
+
+data class EpgProgram(
+    val title: String,
+    val startTime: Long,
+    val endTime: Long
+)
 
 object EpgHelper {
-    private val client = OkHttpClient()
-    private const val EPG_URL = "https://epg.catvod.com/epg.xml"
-
-    suspend fun loadEpg(): MutableList<EpgProgram> {
+    fun parseXml(xmlContent: String): List<EpgProgram> {
         val list = mutableListOf<EpgProgram>()
-        val req = Request.Builder().url(EPG_URL).build()
-        val resp = client.newCall(req).execute()
-        val stream = resp.body?.byteStream() ?: return list
-        val parser = XmlPullParserFactory.newInstance().newPullParser()
-        parser.setInput(stream, "UTF-8")
-        var event = parser.eventType
-        var cid = ""
-        var title = ""
-        var start = ""
-        var stop = ""
-        while(event != parser.END_DOCUMENT){
-            when(event){
-                parser.START_TAG -> {
-                    when(parser.name){
-                        "channel" -> cid = parser.getAttributeValue(null, "id")
-                        "title" -> title = parser.nextText()
-                        "start" -> start = parser.nextText()
-                        "stop" -> stop = parser.nextText()
+        try {
+            val factory = XmlPullParserFactory.newInstance()
+            val parser = factory.newPullParser()
+            parser.setInput(StringReader(xmlContent))
+
+            var eventType = parser.eventType
+            var currentTitle = ""
+            var start = 0L
+            var end = 0L
+
+            while (eventType != XmlPullParser.END_DOCUMENT) {
+                when (eventType) {
+                    XmlPullParser.START_TAG -> {
+                        val tag = parser.name
+                        if (tag == "programme") {
+                            start = parser.getAttributeValue(null, "start")?.toLongOrNull() ?: 0
+                            end = parser.getAttributeValue(null, "stop")?.toLongOrNull() ?: 0
+                        }
+                        if (tag == "title") {
+                            currentTitle = parser.nextText()
+                        }
+                    }
+                    XmlPullParser.END_TAG -> {
+                        if (parser.name == "programme") {
+                            list.add(EpgProgram(currentTitle, start, end))
+                        }
                     }
                 }
-                parser.END_TAG -> {
-                    if(parser.name == "programme"){
-                        list.add(EpgProgram(cid, title, start, stop))
-                    }
-                }
+                eventType = parser.next()
             }
-            event = parser.next()
+        } catch (e: Exception) {
+            e.printStackTrace()
         }
         return list
     }
