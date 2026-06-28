7e0a8c86b833581390182279b16fd1b2755e0354
cuicanrensheng <1693728915@qq.com>
2026-06-28 09:08:47 +0800

    Update PictureInPictureManager.java

--- app/src/main/java/com/tv/live/PictureInPictureManager.java
+++ app/src/main/java/com/tv/live/PictureInPictureManager.java
@@ -14,6 +14,7 @@ import com.tv.live.manager.ChannelPanelController;
 import com.tv.live.manager.DisplayManager;
 import java.lang.ref.WeakReference;
 import java.util.List;
+import com.tv.live.manager.InfoDisplayManager;
 
 /**
  * 画中画(PIP)核心管理器 修复内存泄漏版
