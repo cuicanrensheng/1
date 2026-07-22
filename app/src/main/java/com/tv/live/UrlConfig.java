bb07ac745160fce26d7729248166f3cf759f193f
cuicanrensheng <1693728915@qq.com>
2026-07-22 20:14:27 +0800

    Update UrlConfig.java

--- app/src/main/java/com/tv/live/UrlConfig.java
+++ app/src/main/java/com/tv/live/UrlConfig.java
@@ -1,6 +1,6 @@
 package com.tv.live;
 
 public class UrlConfig {
-    public static String LIVE_URL = "https://gitee.com/qf_1111/iptv/raw/master/playlist.m3u";
+    public static String LIVE_URL = "https://raw.githubusercontent.com/cuicanrensheng/IPTV/refs/heads/main/playlist1.m3u";
     public static String EPG_URL = "https://e.erw.cc/all.xml.gz";
 }
