dca9a8e4bd343a4aba9e0df034e79249f0317513
cuicanrensheng <1693728915@qq.com>
2026-07-23 19:06:05 +0800

    Update UrlConfig.java

--- app/src/main/java/com/tv/live/UrlConfig.java
+++ app/src/main/java/com/tv/live/UrlConfig.java
@@ -2,5 +2,5 @@ package com.tv.live;
 
 public class UrlConfig {
     public static String LIVE_URL = "https://raw.githubusercontent.com/cuicanrensheng/IPTV/refs/heads/main/playlist1.m3u";
-    public static String EPG_URL = "https://e.erw.cc/all.xml.gz";
+    public static String EPG_URL = "https://epg.catvod.com/epg.xml";
 }
